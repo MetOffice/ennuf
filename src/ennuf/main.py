a8c448bfa00c4e3d821816063302a34c96d8cef1
Helena Reid <helena.reid@metoffice.gov.uk>
2023-09-07 09:19:55 +0100

    Removed python parser as the approach of analysing abstract syntax trees rather than model objects has been abandoned.

--- src/ennuf/main.py
+++ src/ennuf/main.py
@@ -1,41 +1,21 @@
 #  (C) Crown Copyright, Met Office, 2023.
-import ast
 
-from ennuf.const.path import KERAS_FUNCTIONAL_EXAMPLES_DIR
-from ennuf.python_parser.keras_functional import KerasFunctionalModelAnalyzer
+from ennuf.formatters.minimalist_formatter import MinimalistFormatter
 from ennuf.simple_mlp import SimpleMLP
 from ennuf.translation.keras.functional import from_keras_functional
-
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def main():
     model = SimpleMLP.build()
     model = from_keras_functional(model)
     print(model)
-    fortran_file = ''
-    fortran_file += model.fortran_file_head()
-    fortran_file += model.fortran_module_head()
-    fortran_file += model.fortran_subroutine()
-    fortran_file += model.fortran_module_tail()
-    with open('test.f90', 'w') as file:
-        file.write(fortran_file)
+    model.create_fortran_module('um_test.f90')
+
+    model.formatter = MinimalistFormatter()
+    model.create_fortran_module('test.f90')
 
     return
-    with open(KERAS_FUNCTIONAL_EXAMPLES_DIR.iterdir().__next__(), 'r') as source:
-        tree = ast.parse(source.read())
-    visitor = KerasFunctionalModelAnalyzer()
-    visitor.visit(tree)  #
-    # visitor.report()
-    return
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
