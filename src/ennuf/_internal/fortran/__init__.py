7c42bdc7bc8eb93d229b9ab26547b94edaf18669
Helena Reid <helena.reid@metoffice.gov.uk>
2023-09-25 13:43:13 +0100

    Added function to copy neural_net_mod.f90 to a desired destination

--- src/ennuf/_internal/fortran/__init__.py
+++ src/ennuf/_internal/fortran/__init__.py
@@ -1,1 +1,9 @@
 #  (C) Crown Copyright, Met Office, 2023.
+import shutil
+from pathlib import Path
+
+
+def copy_neural_net_mod(dest_dir: Path):
+    fortran_dir = Path(__file__).parent
+    neural_net_mod_file = fortran_dir.joinpath('neural_net_mod.f90')
+    shutil.copy(neural_net_mod_file, dest_dir.joinpath('neural_net_mod.f90'))
