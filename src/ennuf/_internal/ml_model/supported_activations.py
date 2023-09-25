17ec44897a82a8ca77f47e2a8f49980452796393
Helena Reid <helena.reid@metoffice.gov.uk>
2023-09-25 13:45:26 +0100

    Added linear activation support

--- src/ennuf/_internal/ml_model/supported_activations.py
+++ src/ennuf/_internal/ml_model/supported_activations.py
@@ -1,6 +1,7 @@
 #  (C) Crown Copyright, Met Office, 2023.
 from typing import Dict, Type
 
+from ennuf._internal.ml_model.activations.linear import Linear
 from ennuf._internal.ml_model.base_activation import BaseActivation
 from ennuf._internal.ml_model.activations.leaky_relu import LeakyRelu
 from ennuf._internal.ml_model.activations.relu import Relu
@@ -11,7 +12,7 @@ from ennuf._internal.ml_model.activations.tanh import Tanh
 class SupportedActivations:
     @staticmethod
     def ids() -> Dict[str, Type[BaseActivation] | None]:
-        return {'relu': Relu, 'sigmoid': Sigmoid, 'tanh': Tanh, 'linear': None, 'LeakyReLU': LeakyRelu}
+        return {'relu': Relu, 'sigmoid': Sigmoid, 'tanh': Tanh, 'linear': Linear, 'LeakyReLU': LeakyRelu}
 
     @classmethod
     def from_identifier(cls, id_: str) -> BaseActivation | None:
