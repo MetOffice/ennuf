423a3fdf195f5c0c5eb4ef1e54fb862a4453b029
Helena Reid <helena.reid@metoffice.gov.uk>
2024-01-11 12:36:39 +0000

    Did some mucking about with testing the t anomaly covariances ml model

--- test/testennuf/kgo/test_keras.py
+++ test/testennuf/kgo/test_keras.py
@@ -1,17 +1,10 @@
-#  (C) Crown Copyright, Met Office, 2023.
-import numpy as np
-from pathlib import Path
+#  (C) Crown Copyright, Met Office, 2024.
 
-import tensorflow as tf
-import tensorflow as tf
-import os.path
 import shutil
 
 import pytest
 
-import ennuf.keras
-from ennuf._internal.formatters import MinimalistFormatter
-from testennuf import TMPDIR, TESTS_ROOT_DIR, WEIGHTS_DIR
+from testennuf import TMPDIR, WEIGHTS_DIR
 from testennuf.example_models.simple_mlp import SimpleMLP
 from testennuf.example_models.t_anomaly_covariances_01 import TAnomalyCovariances01
 from testennuf.kgo.template import template_test_kgo
@@ -67,9 +60,48 @@ def test_t_anomaly_covariances_01():
     weights_dir = WEIGHTS_DIR.joinpath(name)
     keras_model.load_weights(weights_dir)
     inputs = np.asarray([[1.0, 0.12, 0.02, 0.64, 0.9, 0.91]])
-    from ennuf import keras
+    kgo = keras_model.predict(inputs)
 
-    model = ennuf.keras.from_functional(keras_model, name=name.lower(), long_name="T Anomaly Covariances 01")
-    model.formatter = MinimalistFormatter()
-    model.create_fortran_module("/data/users/hreid/src/ENNUF/ref/tac001v001/tac01v001_mod.f90")
+    def get_covariance_matrix(diags):
+        nlev = 70
+        cov = np.zeros((nlev, nlev))
+        for i in range(nlev):
+            cov[i, i] = diags[i]
+        for i in range(nlev - 1):
+            cov[i, i + 1] = diags[nlev + i]
+            cov[i + 1, i] = diags[nlev + i]
+        for i in range(nlev - 2):
+            cov[i, i + 2] = diags[nlev + nlev - 1 + i]
+            cov[i + 2, i] = diags[nlev + nlev - 1 + i]
+        return cov
+
+    def get_covariance_matrix_alternative(diags):
+        def alpha(i, j):
+            if j < 2:
+                return 1
+            else:
+                return 0
+        nlev = 70
+        cov = np.zeros((nlev, nlev))
+        for i in range(nlev):
+            cov[i, i] = diags[i]
+        for j in range(nlev-1):
+            for i in range(nlev - j):
+                cov[i, i + j] = alpha(i, j) * diags[i]*diags[i+j]
+                cov[i + j, i] = alpha(i, j) * diags[i]*diags[i+j]
+        return cov
+
+    A = get_covariance_matrix(kgo[0])
+
+    # FIXME: A should be positive semidefinite but isn't
+    # model = ennuf.keras.from_functional(keras_model, name=name.lower(), long_name="T Anomaly Covariances 01")
+    # model.formatter = MinimalistFormatter()
+    # def check_cholesky_matrix_gives_positive_definite_covariance_matrix(L):
+    #     if np.isclose(np.linalg.det(L), 0):
+    #         raise ValueError('determinant of L may not be zero')
+    #     A = np.matmul(L, L.T)
+    #     rng.multivariate_normal(np.zeros(L.shape[0]), cov=A, check_valid='raise')
+    #     print('all fine')
+    #
+    # model.create_fortran_module("/data/users/hreid/src/ENNUF/ref/tac001v001/tac01v001_mod.f90")
     template_test_keras_functional(keras_model)
