37febaf549533b66ce05e08621f85c7b2de40246
Helena <118900343+MO-Helena-Reid@users.noreply.github.com>
2025-05-29 18:30:51 +0100

    Update svr_mod.f90 to remove commented out code

--- src/ennuf/_internal/fortran/svr_mod.f90
+++ src/ennuf/_internal/fortran/svr_mod.f90
@@ -8,27 +8,6 @@ MODULE svr_mod
 
 IMPLICIT NONE
 
-! ! INTEGER, PARAMETER :: precision=4
-! ! REAL(kind = precision), DIMENSION(2)  :: y_out
-! ! CALL svr(3,2,2,y_out, 4)
-! INTEGER :: n_supp_vectors, n_dims, n_in
-! REAL(kind = 4) :: intercept
-! !real :: supp_vectors(n_supp_vectors,n_dims), dual_coef(n_supp_vectors), x_in(n_in, n_dims), y_out(n_in), temp(n_dims)
-! REAL(kind = 4), DIMENSION(5,1) :: supp_vectors
-! REAL(kind = 4), DIMENSION(5) :: dual_coef
-! REAL(kind = 4), DIMENSION(1,1)  :: x_in
-! REAL(kind = 4), DIMENSION(5)  :: temp
-! REAL(kind = 4), DIMENSION(1)  :: y_out
-! x_in=reshape((/ 0.38 /), shape(x_in))
-! supp_vectors=reshape((/ -1.18184952, 0.2020686, -1.12780997, 1.16483002, -1.23727212 /), shape(supp_vectors))
-! dual_coef=[-91.7370892,   -8.2629108,  100.0       ,  100.0       , -100.0]
-! n_supp_vectors=5
-! n_dims=1
-! n_in=1
-! ! y_out=[0]
-! intercept=-5.39999999
-! CALL svr(x_in, supp_vectors, dual_coef, n_supp_vectors, n_dims, n_in, y_out, intercept)
-
 CONTAINS
 
    SUBROUTINE rbf(n_supp_vectors, n_dims, supp_vectors, x_in, y_out)
